70030cbbb46a560524d919e7cffd9e105bb7b59d
ahmetturkyilmaz <turkyilmazah@gmail.com>
2023-08-17 15:33:28 +0300

    take it

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -5,9 +5,10 @@ export const fetchData = (pool: Client, interval: number): Promise<QueryResult<H
     return pool.query<HealthCheck>(` 
                                                   SELECT *, array_to_json(locations) as locations
                                                   FROM health_check hc
+                                                 
                                                            JOIN health_task_metadata m ON hc."metadataId" = m.id
                                                            JOIN task_insight i ON hc."insightsId" = i.id
-                                                  where hc.interval = $1;`, [interval])
+                                                  where hc.enabled = true AND hc.interval = $1;`, [interval])
 }
 export const updateData = (pool: Client): Promise<any> => {
     return pool.query('UPDATE health_check SET "inProgress" = true where EXTRACT(EPOCH FROM (now() - "lastChecked")) > "interval" AND "inProgress" = false')
